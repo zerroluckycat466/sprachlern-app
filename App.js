9f1903d050b2c30cd6586f8cc403009309b61c75
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-09-03 00:24:15 +0800

    feat: initial commit for App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Willkommen zur Sprachlern-App!</h1>
+        <p>Hier beginnen Sie Ihre Reise zum Sprachen lernen.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
